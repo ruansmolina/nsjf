30f7dd0036ad8ca5cadd15e5d5076b338046585b
Ruan Molina <ruan.molina@hotmail.com>
2026-05-05 00:11:04 -0300

    fix: change dependency react-router to react-router-dom

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,12 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 import { queryClient } from "./queryClient";
 import { router } from "./router";
 
 function Providers () {
     return  ( 
     <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <RouterProvider   router={router} />
             
     </QueryClientProvider> );
 }
